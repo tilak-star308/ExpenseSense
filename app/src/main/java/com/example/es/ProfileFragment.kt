754a253bb1bba655f918d55ef76f2d0001efe04d
amshugannu <amshu3003@gmail.com>
2026-03-13 21:09:59 +0530

    Implemented the Onbaoarding screen and BottomNavigationView Testing completed code ready to push

---
+++ app/src/main/java/com/example/es/ProfileFragment.kt
@@ -0,0 +1,43 @@
+package com.example.es
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
+        
+        btnLogout.setOnClickListener {
+            val auth = FirebaseAuth.getInstance()
+            auth.signOut()
+
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+            googleSignInClient.signOut().addOnCompleteListener {
+                val intent = Intent(requireActivity(), SignUpActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                requireActivity().finish()
+            }
+        }
+        
+        return view
+    }
+}
